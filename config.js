0cbd7955020248144762fa12baf126bbeef7d4f2
dwivedishruti17 <shrutimun17@gmail.com>
2024-09-03 22:11:43 +0530

    change in config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 5000;
+export const PORT = process.env.PORT || 5000;
 
 export const mongoDBURL =
   'mongodb+srv://shrutimun17:4BcvR7m9zL4e29k4@cluster0.a5k1jf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
