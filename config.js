df46018d0f7f50401d656e6d7c9a91580e87153d
dwivedishruti17 <shrutimun17@gmail.com>
2024-09-04 01:02:47 +0530

    chnages

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const PORT = process.env.PORT || 5000;
 
-export const mongoDBURL =
-  'mongodb+srv://shrutimun17:4BcvR7m9zL4e29k4@cluster0.a5k1jf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+export const mongoDBURL = process.env.MONGODB_URL;
